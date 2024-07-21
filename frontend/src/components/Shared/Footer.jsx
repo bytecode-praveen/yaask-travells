3e3d450e13228f5f74060b82af1973de551fb022
vikasyaask <yaaskconsulting@gmail.com>
2024-07-21 21:40:40 +0530

    fix

--- frontend/src/components/Shared/Footer.jsx
+++ frontend/src/components/Shared/Footer.jsx
@@ -6,39 +6,43 @@ const Footer = () => {
     <footer className=" py-12 bg-[#f7f7f7] border-t border-[#dddddd] text-sm text-[#222222] relative bottom-0 z-[20]">
       <section className=" grid grid-cols-2 md:grid-cols-4 gap-8 justify-between max-w-screen-2xl mx-auto px-10">
         <div className="flex flex-col gap-4 opacity-80">
-           <a href="http://localhost:3001">
+           <a href="mailto:support@hotelbox.in">
             <h6 className="font-semibold">Support</h6>
           </a> 
-          <a href="http://localhost:3001">
+          <a href="mailto:info@hotelbox.in">
             <p>Help Center</p>
           </a>
-          <a href="http://localhost:3001">
-            <p>Get help with a safety issue</p>
+          <a href="tel:+91 8141488868">
+            <p>+91 8141488868</p>
           </a>
-          <a href="http://localhost:3001">
-            <p>MotelCover</p>
+          <a href="#">
+            <p>Address: </p>
+            <p>LS No.122, Rajpath Rangoli Rd,
+            near Shivalik Villa, beside Aangan Farm Cross Road, Ambli, Ahmedabad, Gujarat 380058</p>
           </a>
-          <a href="http://localhost:3001">
-            <p>Supporting people with disabilities</p>
-          </a>
-          <a href="http://localhost:3001">
-            <p>Cancelation options</p>
-          </a>
-          <a href="https://covid19-sg.netlify.app">
-            <p>Our Covid-19 response</p>
-          </a>
-          <a href="http://localhost:3001">
-            <p>Report a neighborhood concern</p>
-          </a>
+         
+          
         </div>
         <div className="flex flex-col gap-4 opacity-80">
-          <h6 className="font-semibold">Community</h6>
-          <a href="http://localhost:3001">
-          <p>Motel.org: Disaster relief housing</p>
-          </a>
-          <a href="http://localhost:3001">
-          <p>Combating discrimination</p>
-            </a>
+          <h6 className="font-semibold">Top Brands</h6>
+          
+          <p>WelcomHotel</p>
+          
+         
+          <p>Fairmont</p>
+           
+          
+          <p>Radisson</p>
+            
+            
+          <p>Lemon Tree</p>
+            
+           
+          <p>Marriot</p>
+            
+            
+          <p>The Lalit</p>
+            
         </div>
         <div className="flex flex-col gap-4 opacity-80">
           <h6 className="font-semibold">Hosting</h6>
@@ -60,31 +64,31 @@ const Footer = () => {
 <a href="http://localhost:3001/host/homes">
     <p>Motel friendly apartments</p>
 </a>
-        </div>
-        <div className="flex flex-col gap-4 opacity-80">
-          <h6 className="font-semibold">Motel</h6>
-          <p>Newsroom</p>
-          <a href="http://localhost:3001/host/homes">
-    <p>MotelCover for Hosts</p>
+</div>
+<div className="flex flex-col gap-4 opacity-80">
+  <h6 className="font-semibold">Popular Cities</h6>
+  <p>Gujrat</p>
+  <a href="http://localhost:3001/host/homes">
+  <p>Udaipur</p>
 </a>
 <a href="http://localhost:3001/host/homes">
-    <p>Explore hosting resources</p>
+    <p>Jaipur</p>
 </a>
 <a href="http://localhost:3001/host/homes">
-    <p>Visit our community forum</p>
+    <p>Goa</p>
 </a>
 <a href="http://localhost:3001/host/homes">
-    <p>How to host responsibly</p>
+    <p>Jodhpur</p>
 </a>
 <a href="http://localhost:3001/host/homes">
-    <p>Motel friendly apartments</p>
+    <p>Mumbai</p>
 </a>
         </div>
       </section>
       <hr className="bg-[#f7f7f7] mt-10 mb-6" />
       <section className=" flex flex-row flex-wrap justify-between gap-10 px-10 max-w-screen-2xl mx-auto">
         <div className=" flex flex-row flex-wrap items-center">
-          <p>© 2024 Yaask Travells, Inc.</p>
+          <p>© 2024 HotelBox LLP.</p>
           <span className=" p-3">·</span>
           <p>Terms</p>
           <span className=" p-3">·</span>
@@ -94,10 +98,10 @@ const Footer = () => {
         </div>
         <div className=" flex flex-row gap-5 min-w-[120px] items-center">
           <p>English (US)</p>
-          <a href="https://www.facebook.com">
+          <a href="https://www.facebook.com/hotelbox.in">
             <img src={facebookIcon} alt="Facebook" className=" w-6" />
           </a>
-          <a href="https://www.linkedin.com/in/sk-mirajul-islam-876438261/">
+          <a href="https://www.linkedin.com/company/hotelbox-llp">
             <img src={linkedinIcon} alt="Linkedin" className=" w-6" />
           </a>
         </div>
